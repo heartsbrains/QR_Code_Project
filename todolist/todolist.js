b4154a7b16e02d0fccdb2aa702de4280230a9866
Akshay Teli <132123637+heartsbrains@users.noreply.github.com>
2023-12-12 11:05:07 +0530

    Add files via upload

---
+++ todolist/todolist.js
@@ -0,0 +1,71 @@
+const addForm = document.querySelector(".add");
+const tasks = document.querySelector(".tasks");
+const clearAll = document.querySelector(".clear");
+const messageSpan = document.querySelector(".message span");
+const searchForm = document.querySelector(".search");
+
+function updateMessage() {
+  const textLength = tasks.children.length;
+
+  messageSpan.textContent = `You Have ${textLength} Pending Tasks`;
+}
+
+addForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const value = addForm.task.value.trim();
+
+  if (value.length) {
+    tasks.innerHTML += ` <li>
+        <span>${value}</span>
+        <i class="bi bi-trash delete"></i>
+    </li>`;
+
+    addForm.reset();
+    updateMessage();
+  }
+});
+
+tasks.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete")) {
+    event.target.parentElement.remove();
+  }
+});
+
+clearAll.addEventListener("click", (event) => {
+  const taskItems = tasks.querySelectorAll("li");
+  taskItems.forEach((item) => {
+    item.remove();
+  });
+  updateMessage();
+});
+
+function filterTask(term) {
+  Array.from(tasks.children)
+    .filter((task) => {
+      return !task.textContent.includes(term);
+    })
+    .forEach((task) => {
+      task.classList.add("hide");
+    });
+
+  Array.from(tasks.children)
+    .filter((task) => {
+      return task.textContent.includes(term);
+    })
+    .forEach((task) => {
+      task.Classlist.remove("hide");
+    });
+}
+
+searchForm.addEventListener("keyup", (event) => {
+  const term = searchForm.task.value.trim();
+  filterTask(term);
+});
+
+searchForm.addEventListener("click", (event) => {
+  if (event.target.classList.contains("reset")) {
+    searchForm.reset();
+    const term = searchForm.task.value.trim();
+    filterTask(term);
+  }
+});
