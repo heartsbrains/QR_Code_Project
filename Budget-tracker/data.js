11922c4262877771c1e7965612df392ea4d067e5
Akshay Teli <132123637+heartsbrains@users.noreply.github.com>
2023-12-15 21:01:00 +0530

    Add files via upload

---
+++ Budget-tracker/data.js
@@ -0,0 +1,7 @@
+const transactions = [
+    {id: 123, source: "Freelancing", amount:499, time: "4:40:38 AM 5/19/2022"},
+    {id: 124, source: "Studio", amount: -49, time:"5:40:38 5/19/2022" },
+    {id:125, source:"cash", amount:500, time:"7:40:38 AM 5/19/2022"},
+    {id:126,source:"Recording", amount:200, time:"9:40:38 AM 5/19/2022"},
+    
+];
